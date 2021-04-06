792aae3a2e0a6925adb5f2a820c9396c8160949d
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-05 22:43:44 -0600

    Add model validations for application form

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,10 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, length: { is: 2 }
+  validates :zipcode, numericality: true
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Application, type: :model do
     it { should have_many :pet_applications}
     it { should have_many(:pets).through(:pet_applications) }
   end
+
+  describe 'validations' do
+     it { should validate_presence_of(:name) }
+     it { should validate_presence_of(:street_address) }
+     it { should validate_presence_of(:city) }
+     it { should validate_length_of(:state).is_equal_to(2) }
+     it { should validate_numericality_of(:zipcode) }
+   end
 end
